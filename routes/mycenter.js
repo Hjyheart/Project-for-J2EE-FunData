c6818c23e15c25f5ae77fce9d1e912e5c383d4ef
huang <1181591811hzr@gmail.com>
2016-11-23 22:50:12 +0800

    finish the basically route

--- routes/mycenter.js
+++ routes/mycenter.js
@@ -4,29 +4,67 @@
 
 var express = require('express');
 var router = express.Router();
+var _const = require('../const');
 
 /* MyCenter Profile*/
-router.get('/profile', function (req, res, next) {
-   res.render('./mycenter/profile', {title : 'profile'});
+router.get('/profile/:email', function (req, res, next) {
+    request({
+        url: `${_const.ServerHost}/profile`,
+        method: 'POST',
+        form: {
+            name: req.params.email
+        }
+    }, function (err, response, body) {
+        console.log(body);
+    });
+    res.render('./mycenter/profile', {title : 'profile'});
 });
 
 /* MyCenter Information */
 router.get('/information', function (req, res, next) {
-   res.render('./mycenter/information', {title : 'information'});
+
+    res.render('./mycenter/information', {title : 'information'});
 });
 
 /* MyCenter MyDataset */
-router.get('/mydataset', function (req, res, next) {
+router.get('/mydataset/:email', function (req, res, next) {
+    request({
+        url: `${_const.ServerHost}/dataset`,
+        method: 'POST',
+        form: {
+            email: req.params.email
+        }
+    }, function (err, response, body) {
+        console.log(body);
+    });
     res.render('./mycenter/mydataset', {title : 'mydataset'});
 });
 
 /* MyCenter MyCompetition */
-router.get('/mycompetition', function (req, res, next) {
+router.get('/mycompetition/:email', function (req, res, next) {
+    request({
+        url: `${_const.ServerHost}/competition`,
+        method: 'POST',
+        form: {
+            email: req.params.email
+        }
+    }, function (err, response, body) {
+        console.log(body);
+    });
     res.render('./mycenter/mycompetition', {title : 'mycompetition'});
 });
 
 /* MyCenter MyMooc */
-router.get('/mymooc', function (req, res, next) {
+router.get('/mymooc/:email', function (req, res, next) {
+    request({
+        url: `${_const.ServerHost}/mooc`,
+        method: 'POST',
+        form: {
+            email: req.params.email
+        }
+    }, function (err, response, body) {
+        console.log(body);
+    });
     res.render('./mycenter/mymooc', {title : 'mymooc'});
 });
 
