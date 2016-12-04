f294427b0318fba386d6ab8d944bfad0a67dd486
洪嘉勇 <tjhjyisland@gmail.com>
2016-12-04 17:09:24 +0800

    做完了mooc 的 home

--- public/controllers/mooc/homeCtrl.js
+++ public/controllers/mooc/homeCtrl.js
@@ -2,30 +2,90 @@
  * Created by hongjiayong on 2016/12/3.
  */
 var app = angular.module('myApp', []);
-app.controller('homeCtrl', function ($scope, $http) {
-    // $scope.screen_hot_course;
+app.controller('homeCtrl',[ '$scope', '$http', function ($scope, $http) {
+    $scope.screen_hot_course;
     $scope.mooc_title;
-    // $scope.boutique_course;
+    $scope.boutique_course;
     // $scope.other_course;
 
     this.$onInit = function () {
        $http({
            method: 'POST',
-           url: 'http://127.0.0.1:8080/mooc/init',
+           url: 'http://localhost:8080/mooc/init'
        }).then( res =>{
-           $scope.mooc_title = res.data;
+           $scope.screen_hot_course = res.data.screen_hot_course;
+           $scope.boutique_course = res.data.boutique_course;
+           $scope.other_course = res.data.other_course;
+       }).catch(err =>{
+           console.log(err);
        });
+
+
     };
 
-    // this.getList = function(url, pageStart) {
-    //     return $http({
-    //         method: "POST",
-    //         url: url,
-    //         params: {
-    //             "user_id": infoService.getInfo('user_id'),
-    //             "pageStart": pageStart
-    //         }
-    //     });
-    // }
+    $scope.turnToDetail = function (course) {
+        window.location.href = '/mooc/' + course.course_id + '/detail';
+    };
 
-});
+    $scope.freshCourseOthers = function () {
+        $('#freshCourseOthersBtn').addClass('loading');
+        var end = $('#other-course-container').children().length;
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/mooc/fresh-other',
+            params: {
+                'current': end
+            }
+        }).then(res =>{
+            for (var i = 0; i < res.data.other_course.length; i++){
+                $scope.other_course.push({
+                    'course_id': res.data.other_course[i].course_id,
+                    'course_name': res.data.other_course[i].course_name,
+                    'course_join_sum': res.data.other_course[i].course_join_sum,
+                    'course_teacher_name':res.data.other_course[i].course_teacher_name,
+                    'course_teacher_id': res.data.other_course[i].course_teacher_id,
+                    'course_des': res.data.other_course[i].course_des
+                });
+            }
+        }).catch(err =>{
+            console.log(err);
+        });
+
+        setTimeout(function () {
+            $('#freshCourseOthersBtn').removeClass('loading');
+        }, 1000);
+    };
+
+    $scope.freshCourseBoutique = function () {
+        $("#freshCourseBoutiqueBtn").addClass('loading');
+        var end = $('#boutique-course-container').children().length;
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/mooc/fresh-boutique',
+            params:{
+                'current': end
+            }
+        }).then( res =>{
+
+            for (var i = 0; i < res.data.boutique_course.length; i++){
+                $scope.boutique_course.push({
+                    'course_id': res.data.boutique_course[i].course_id,
+                    'course_name': res.data.boutique_course[i].course_name,
+                    'course_join_sum': res.data.boutique_course[i].course_join_sum,
+                    'course_teacher_name':res.data.boutique_course[i].course_teacher_name,
+                    'course_teacher_id': res.data.boutique_course[i].course_teacher_id,
+                    'course_des': res.data.boutique_course[i].course_des
+                });
+            }
+        }).catch(err =>{
+            console.log(err);
+        })
+        setTimeout(function () {
+            // 在这里刷新mooc
+            $("#freshCourseBoutiqueBtn").removeClass('loading');
+        }, 1000);
+    };
+
+
+
+}]);
