a40b173ee669e3c47ef731c67d0e266b5295aa94
洪嘉勇 <tjhjyisland@gmail.com>
2016-12-05 17:01:31 +0800

    还差comment

--- public/controllers/mooc/detailCtrl.js
+++ public/controllers/mooc/detailCtrl.js
@@ -8,18 +8,36 @@ app.controller('detailCtrl',
     var course_name = $('#course_name').text();
     var course_id = $('#course_id').text();
 
+    $scope.steps;
 
-    $http({
-        method: 'POST',
-        url: 'http://localhost:8080/mooc/' + course_id + '/' + course_name + '/detail',
-    })
-        .then( res =>{
-            var overview = res.data.course_detail.course_overview.overview_content;
-            $('#overview-content').replaceWith(divideService.getHtml(overview));
-
+    this.$onInit = function () {
+        $http({
+            method: 'POST',
+            url: 'http://localhost:8080/mooc/' + course_id + '/' + course_name + '/detail',
         })
-        .catch(err =>{
-           console.log(err);
-        });
+            .then( res =>{
+                var overview = res.data.course_detail.course_overview.overview_content;
+                $('#overview-content').replaceWith(divideService.getHtml(overview));
+
+                var steps = res.data.course_detail.course_steps;
+
+                $scope.steps = steps;
+
+            })
+            .catch(err =>{
+                console.log(err);
+            });
+    };
+
+    $scope.showStep = function (step, index) {
+
+        $('#' + index).replaceWith(divideService.getHtml(step.step_content));
+
+
+        $('#overview-btn').removeClass('active');
+        $('#comments-btn').removeClass('active');
+        $('.ui.attached.tab.active').removeClass('active');
+        $('#' + step.step_header).addClass('active');
+    };
 
 }]);
