2bd67d43ca68b0692c9249df992d190446401f86
洪嘉勇 <tjhjyisland@gmail.com>
2016-12-08 17:36:48 +0800

    完成mooc

--- public/controllers/mooc/manageCtrl.js
+++ public/controllers/mooc/manageCtrl.js
@@ -16,7 +16,15 @@ app.controller('manageCtrl', ['$scope', '$http', 'constService', 'divideService'
                 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus',
                 'step_pic_url': '/images/wudi.jpg'
             }
-        ]
+        ];
+
+        $scope.overview = {
+            'overview_content': 'Pellentesque /n habitant morbi tristique senectus et netus et malesuada /n fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, ' +
+            'metus Pellentesque habitant morbi tristique senectus et netus et malesuada /n fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus' +
+            'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus'
+        };
+
+        $('#overview-content').replaceWith(divideService.getHtml($scope.overview.overview_content));
     };
 
     $scope.showStep = function (step, index) {
@@ -35,6 +43,25 @@ app.controller('manageCtrl', ['$scope', '$http', 'constService', 'divideService'
     $scope.stepSubmit = function (step, index) {
         $('#' + index + '-edit-form').transition('vertical flip');
         $('#' + index + '-edit-btn').transition('fly left');
+    };
+
+    $scope.overEditDeal = function () {
+        $('#over-edit-form').transition('vertical flip');
+        $('#over-edit-content').text($('#over-container').text());
+        $('#over-edit-btn').transition('fly left');
+    };
+
+    $scope.overSubmit = function () {
+        $('#over-edit-form').transition('vertical flip');
+        $('#over-edit-btn').transition('fly left');
+    };
+
+    $scope.changeGuide = function (step) {
+        $('#new-guide').modal('show');
+    };
+
+    $scope.addNewStep = function () {
+        $('#add-step').modal('show');
     };
 
 }]);
