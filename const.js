a6de8e9bb599c5ed43bbc02a8260efde57543d2a
洪嘉勇 <tjhjyisland@gmail.com>
2016-12-14 16:29:15 +0800

    update competition

--- const.js
+++ const.js
@@ -2,7 +2,7 @@
  * Created by huang on 16-11-23.
  */
 const obj = {
-    ServerHost:'http://192.168.1.9:8080',
+    ServerHost:'http://10.60.42.202:8080',
     pageSize: 4,
     sessionSecret: 'fundata',
     RedisHost: '10.60.42.201',
